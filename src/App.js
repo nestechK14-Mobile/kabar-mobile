5e375db012cf70ae13c4009cb36447a5a1640fc4
MinhTuan89 <tuanbk89@gmail.com>
2023-10-29 18:09:12 +0700

    1. Move Source File to Root folder 2. Config Splash+AppIcon+AppName -- 100%

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { LogBox, StyleSheet, Text, View } from 'react-native';
-// import SplashScreen from 'react-native-splash-screen';
-import React from 'react';
+import { StyleSheet, Text, View, useColorScheme} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+import React,{ useEffect } from 'react';
 import MainRouter from './routers/mainRouter';
 import { NavigationContainer } from '@react-navigation/native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 // LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
 // LogBox.ignoreAllLogs();//Ignore all log notifications
 
 const App = () => {
-  // useEffect(() => {
-  //   SplashScreen.hide();
-  // }, []);
+
+
+
+  useEffect(() => {
+   
+    SplashScreen.hide();
+  }, []);
   return (
+    
     <NavigationContainer>
       <MainRouter />
     </NavigationContainer>
