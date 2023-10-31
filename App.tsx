5e0f5bc47735a07d81a463cfcdafb8bdd4f87fce
Kien1509 <66570324+kienwewdwd@users.noreply.github.com>
2023-10-31 19:03:42 +0700

    newConfig

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import { LogBox, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
 LogBox.ignoreAllLogs();//Ignore all log notifications
 
 const App = () => {
 
   return (
-    <NavigationContainer>
-      <MainRouter />
-    </NavigationContainer>
+    <View style = {{flex: 1}}>
+      <Text>
+        Hello!!!!!
+      </Text>
+    </View>
+
   );
 };
 
