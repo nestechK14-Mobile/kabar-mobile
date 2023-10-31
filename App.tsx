764249fffb783fa9b982a7203f49f7c73e0da2f6
Nam Nguyen <namnh98@gmail.com>
2023-10-31 18:44:59 +0700

    config/main-source: update source base - init source

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'black' }}>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
