40c0cf09ceb161e10fdd821cd218086264cf127e
MinhTuan89 <tuanbk89@gmail.com>
2023-10-31 19:44:39 +0700

    Update-3-31-10

--- src/assets/image/index.js
+++
@@ -1,2 +0,0 @@
-// export { default as IMG_AppLogo } from './IMG_AppLogo.png';
-// export { default as IMG_Google } from './IMG_Google.png';
--- src/screen/index.js
+++
@@ -1,6 +0,0 @@
-export { default as HomeScreen} from './home';
-export { default as ExploreScreen} from './explore';
-export { default as ProfileScreen} from './profile';
-export { default as SettingScreen} from './setting';
-export { default as AuthencationScreen} from './authencation';
-export { default as UserScreen} from './user';
