f8788a3ee5045c80025efef0e751864cfc237839
Kien1509 <66570324+kienwewdwd@users.noreply.github.com>
2023-11-05 19:12:48 +0700

    CustomIputTextFiled

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import { Text, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import React, { useEffect } from 'react';
-import { FONTS, IC_Back, IMG_Art } from '@/assets';
+import React, { useEffect, useState } from 'react';
+import { FONTS, IC_Back, IC_Bell, IMG_Art, IMG_Avatar, IMG_KabarLogo } from '@/assets';
 import { COLORS } from '@/constants';
+import CustomButton from '@/component/common/customButton';
+import CustomInputField from '@/component/common/customInputField';
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
-
+  const [textInput, setTextInput] = useState('');
+  
+  const handleClearPress = () => {
+    setTextInput(''); // Xóa nội dung khi nút "Clear" được nhấn
+  };
   return (
     <View style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center' }}>
       <Text style={{ color: COLORS.BLACK, fontFamily: FONTS.REGULAR }}>HomeScreen-by MinhTuan</Text>
       <IC_Back />
+      <CustomButton>
+      <Text>dddd</Text>
+      <IMG_Avatar/>
+      <IC_Bell/>
+      </CustomButton>
       <IMG_Art />
+      <CustomInputField title="Password" secureTextEntry onChangeText={setTextInput} value={textInput} />
+      <CustomInputField  onChangeText={setTextInput} value={textInput} />
+
+
     </View>
   );
 };
---
+++ src/component/common/customButton/index.js
@@ -0,0 +1,27 @@
+import {
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback
+} from 'react-native';
+import React from 'react';
+
+const CustomButton = ({ children, onPress, type,  ...rest }) => {
+  const getTouchableComponent = () => {
+    switch (type) {
+      case 'highlight':
+        return TouchableHighlight;
+      case 'withoutFeedback':
+        return TouchableWithoutFeedback;
+      default:
+        return TouchableOpacity;
+    }
+  };
+  const TouchableComponent = getTouchableComponent();
+  return (
+    <TouchableComponent onPress={onPress} {...rest}>
+    {children}
+    </TouchableComponent>
+  );
+};
+
+export default CustomButton;
