c26b78ce904f52710363853dedbc10eb84c3b829
Nam Nguyen <namnh98@gmail.com>
2023-12-14 20:59:32 +0700

    update new way handle error

--- src/screens/signUp/SignUp.container.js
+++ src/screens/signUp/SignUp.container.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import SignUpView from './SignUp.view';
 import { PAGE_CONSTANTS } from '@/constants';
 
@@ -6,17 +6,22 @@ const SignUpContainer = () => {
   const { CONTENT_LABEL } = PAGE_CONSTANTS.AUTHENTICATION;
   const { SIGN_UP } = CONTENT_LABEL;
   const [userName, setUserName] = useState('');
-  const [errorText, setErrorText] = useState('');
 
   const onChangeValueUserName = value => {
     setUserName(value);
   };
 
   const handleSignUp = () => {
-    if (!userName) {
-      setErrorText('Cần nhập UserName!');
+    if (errorText) {
+      console.log('làm tiếp');
     }
   };
+
+  const errorText = useMemo(() => {
+    if (!userName) {
+      return 'Cần nhập UserName!';
+    }
+  }, [userName]);
 
   const propsSignUp = {
     onChangeValueUserName,
