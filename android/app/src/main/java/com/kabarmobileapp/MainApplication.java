7fee79a9ea6108417fba42870f69c8a044cc8ca9
Nguyen Hoang Nam <namnh98@gmail.com>
2024-05-10 08:07:26 +0700

    update config water melon

--- android/app/src/main/java/com/kabarmobileapp/MainApplication.java
+++ android/app/src/main/java/com/kabarmobileapp/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.nozbe.watermelondb.WatermelonDBPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = {
         }
       }
     ],
-    'react-native-reanimated/plugin'
+    'react-native-reanimated/plugin',
+    ['@babel/plugin-proposal-decorators', { legacy: true }]
   ]
 };
